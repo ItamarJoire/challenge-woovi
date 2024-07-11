e0c16af21c9b840100b783bac9e6933ea9502d1e
Itamar Joire <ijoire7@gmail.com>
2024-07-11 05:56:19 -0300

    Spacing adjustment

--- src/containers/gridContainer.jsx
+++ src/containers/gridContainer.jsx
@@ -1,6 +1,6 @@
 export function GridContainer({ children }){
   return(
-    <div className='mx-auto max-w-[440px] mt-9'>
+    <div className='mx-auto max-w-[440px] mt-9 px-2'>
       {children}
     </div>
   )
