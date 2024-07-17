f6049a70c5cb4e3e0d0de336fded37b39d2973b1
itamar-joire <itamar@mirasys.com.br>
2024-07-17 19:40:53 -0300

    paddings change

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,6 +1,6 @@
 export function Grid({ children }){
   return(
-    <div className='mx-auto max-w-[440px] mt-9 px-6 h-screen'>
+    <div className='mx-auto max-w-[440px] mt-9 px-4 h-screen'>
       {children}
     </div>
   )
